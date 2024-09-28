c6d476dc352ecb38bd0a51c5b7dd6d748c272c1f
János Barnabás Restye <barna0801@gmail.com>
2024-09-28 14:40:22 +0200

    rearranging prog & prog Hazi3

---
+++ 1/.prog/kod/negyzetszam/negyzetszam/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace negyzetszam {
+    internal class Program {
+        static void Main(string[] args) {
+            // deklaracio
+            int n;
+            int nszo;
+            int i;
+
+            // beolvasas
+            Console.Write("n=");
+            n = int.Parse(Console.ReadLine());
+
+            // megoldas
+            nszo = 0;
+            for (i = 1; i <= n; ++i) {
+                if(i * i <= n) {
+                    nszo = nszo + i * i;
+                }
+            }
+
+            // kiiras
+            Console.Write("nszo={0}", nszo);
+        }
+    }
+}
