c32752dff39c649fc07fe6c3558f27c7940f8324
János Barnabás Restye <barna0801@gmail.com>
2024-10-08 18:09:56 +0200

    prog fagyos napok done

---
+++ 1/.prog/kod/fagyosNapok/fagyosNapok/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace fagyosNapok {
+    internal class Program {
+        static void Main(string[] args) {
+            // deklaralas
+            int n, fagy, i, j;
+            int[,] ho;
+
+            // beolvasas
+            int.TryParse(Console.ReadLine(), out n);
+            ho = new int[n, 2];
+
+            for (i = 1; i <= n; ++i) {
+                string[] input = Console.ReadLine().Split(' ');
+
+                ho[i - 1, 0] = int.Parse(input[0]);
+                ho[i - 1, 1] = int.Parse(input[1]);
+            }
+
+            // feladat megoldas
+            fagy = 0;
+            for(i = 1; i <= n; ++i) {
+                if (ho[i - 1, 0] < 0 || ho[i - 1, 1] < 0) {
+                    fagy = fagy + 1;
+                }
+            }
+
+            // kiiras
+            Console.WriteLine(fagy);
+        }
+    }
+}
