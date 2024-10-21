fa4c34b09d7bd2037dec85cb538b626d7ec36ff8
János Barnabás Restye <barna0801@gmail.com>
2024-10-21 09:59:35 +0200

    prog beadando kod

--- 1/.prog/kod/fagyosNapok/fagyosNapok/Program.cs
+++ 1/.prog/kod/fagyosNapok/fagyosNapok/Program.cs
@@ -1,27 +1,29 @@
 ﻿using System;
 
 namespace fagyosNapok {
+    public struct Idojaras {
+        public int min, max;
+    }
     internal class Program {
         static void Main(string[] args) {
             // deklaralas
             int n, fagy, i, j;
-            int[,] ho;
 
             // beolvasas
             int.TryParse(Console.ReadLine(), out n);
-            ho = new int[n, 2];
+            Idojaras[] ho = new Idojaras[n];
 
             for (i = 1; i <= n; ++i) {
                 string[] input = Console.ReadLine().Split(' ');
 
-                ho[i - 1, 0] = int.Parse(input[0]);
-                ho[i - 1, 1] = int.Parse(input[1]);
+                ho[i - 1].min = int.Parse(input[0]);
+                ho[i - 1].max = int.Parse(input[1]);
             }
 
             // feladat megoldas
             fagy = 0;
             for(i = 1; i <= n; ++i) {
-                if (ho[i - 1, 0] < 0 || ho[i - 1, 1] < 0) {
+                if (ho[i - 1].min < 0) {
                     fagy = fagy + 1;
                 }
             }
--- 1/.prog/kod/fagyosNapok/fagyosNapok/obj/Debug/net8.0/fagyosNapok.AssemblyInfo.cs
+++ 1/.prog/kod/fagyosNapok/fagyosNapok/obj/Debug/net8.0/fagyosNapok.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fagyosNapok")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54e5d7cd08670856daff39883ac15cd484dd670a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6feec0b596cb97524d374985093a05af7cd6776")]
 [assembly: System.Reflection.AssemblyProductAttribute("fagyosNapok")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fagyosNapok")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
