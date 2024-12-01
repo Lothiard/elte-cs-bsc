d41e3ba3b4f57baf0bda206712a099b3818813ae
János Barnabás Restye <barna0801@gmail.com>
2024-12-01 17:19:36 +0100

    Update BeadSkeleton.hs

--- 1/.funkcProg/beadando/BeadSkeleton.hs
+++ 1/.funkcProg/beadando/BeadSkeleton.hs
@@ -73,7 +73,7 @@ parseTokens opTable input = traverse id $ tokenize [] input
           (num, maradek) -> Just (TokLit (read num)) : tokenize temp maradek
       | otherwise =                                                                 -- ha op
         case lookup x opTable of 
-          Just (op, prec, dir) -> Just (TokBinOp op x prec dir) : tokenize temp xs
+          Just (op, prec, dir) -> Just (TokBinOp op x prec dir) : tokenize temp xs  -- ha nem letezo op
           Nothing -> [Nothing]
   
 parse :: String -> Maybe [Tok Double]
@@ -95,11 +95,10 @@ shuntingYardBasic tokens = shunt tokens [] []
         where
           (newLit, newOp) = eval lit op
 
-    eval lit (BrckOpen : ops) = (lit, ops)
+    eval lit (BrckOpen : ops) = (lit, ops)                   -- alapeset ( '(' az op teteje )
     eval (x2 : x1 : xs) (TokBinOp f _ _ _ : ops) = eval (result : xs) ops
       where
-        result = f x1 x2
-      
+        result = f x1 x2 
 
 syNoEval :: String -> Maybe ([Double], [Tok Double])
 syNoEval = parseAndEval parse shuntingYardBasic
@@ -108,15 +107,125 @@ syEvalBasic :: String -> Maybe ([Double], [Tok Double])
 syEvalBasic = parseAndEval parse (\t -> shuntingYardBasic $ BrckOpen : (t ++ [BrckClose]))
 
 -- 5.
--- shuntingYardPrecedence :: [Tok a] -> ([a], [Tok a])
+shuntingYardPrecedence :: [Tok a] -> ([a], [Tok a])
+shuntingYardPrecedence tokens = shunt tokens [] []
+  where
+    shunt [] lit op = eval lit op
+    shunt (TokLit x : xs) lit op = shunt xs (x : lit) op                      -- 1, lit eseten -> lit
+    shunt (BrckOpen : xs) lit op = shunt xs lit (BrckOpen : op)               -- 2, ( eseten -> op
+    shunt (BrckClose : xs) lit ((TokBinOp f _ _ _):ops) = shunt xs newLit ops -- 3, ) es op list tetejen op van
+      where
+        (x2:x1:rest) = lit      -- elso ket szam
+        newLit = f x1 x2 : rest -- kiszamolo
+    shunt (BrckClose : xs) lit (BrckOpen:ops) = shunt xs lit ops              -- 4, ) es op list tetejen ( van
+    
+    shunt (curr@(TokBinOp _ _ p1 d1) : xs) lit ((TokBinOp f _ p2 _):ops)      -- 5, eltero prec
+      | p1 < p2 || (p1 <= p2 && d1 == InfixL) = shunt (curr:xs) newLit ops
+      where
+        (x2:x1:rest) = lit      -- elso ket szam
+        newLit = f x1 x2 : rest -- kiszamolo
+    shunt (op:xs) lit ops = shunt xs lit (op:ops)
 
--- syEvalPrecedence :: String -> Maybe ([Double], [Tok Double])
--- syEvalPrecedence = parseAndEval parse (\t -> shuntingYardPrecedence $ BrckOpen : (t ++ [BrckClose]))
+    -- helper eval
+    eval lit [] = (lit, [])
+    eval lit (BrckOpen:ops) = eval lit ops              -- 1, ( atugorjuk
+    eval lit (BrckClose:ops) = eval lit ops             -- 2, ) atugorjuk
+    eval lit ((TokBinOp f _ _ _):ops) = eval newLit ops -- 3, szamolas
+      where
+        (x2:x1:rest) = lit
+        newLit = f x1 x2 : rest
+        
+syEvalPrecedence :: String -> Maybe ([Double], [Tok Double])
+syEvalPrecedence = parseAndEval parse (\t -> shuntingYardPrecedence $ BrckOpen : (t ++ [BrckClose]))
 
 -- eqError-t vedd ki a kommentből, ha megcsináltad az 1 pontos "Hibatípus definiálása" feladatot
--- eqError = 0 -- Mágikus tesztelőnek szüksége van rá, NE TÖRÖLD!
+eqError = 0 -- Mágikus tesztelőnek szüksége van rá, NE TÖRÖLD!
 
-{-
+-- 1. hibatipus
+data ShuntingYardError =
+  OperatorOrClosingParenExpected |
+  LiteralOrOpeningParenExpected |
+  NoClosingParen |
+  NoOpeningParen |
+  ParseError
+    deriving (Show, Eq)
+
+type ShuntingYardResult a = Either ShuntingYardError a
+
+-- 2. parseSafe
+bindE :: Either e a -> (a -> Either e b) -> Either e b
+bindE (Left e) _ = Left e
+bindE (Right a) f = f a
+
+parseSafe :: Read a => OperatorTable a -> String -> Either ErrorType [Tok a]
+parseSafe opTable input = tokenize [] input ExpectLit []
+  where
+    data ExpectNext = ExpectLit | ExpectOp
+
+    tokenize :: [Tok a] -> String -> ExpectNext -> [Char] -> Either ErrorType [Tok a]
+    tokenize temp [] expect stack
+      | not (null stack) = Left NoClosingParen
+      | expect == ExpectOp = Right (reverse temp)
+      | otherwise = Left OperatorOrClosingParenExpected
+
+    tokenize temp (x:xs) expect stack
+      | isSpace x = tokenize temp xs expect stack
+
+      -- Handle opening parenthesis '('
+      | x == '(' =
+          if expect == ExpectLit
+            then tokenize (BrckOpen : temp) xs ExpectLit ('(' : stack)
+            else Left LiteralOrOpeningParenExpected
+
+      -- Handle closing parenthesis ')'
+      | x == ')' =
+          if null stack
+            then Left NoOpeningParen
+            else
+              let stack' = tail stack
+              in if expect == ExpectOp
+                    then tokenize (BrckClose : temp) xs ExpectOp stack'
+                    else Left OperatorOrClosingParenExpected
+
+      -- Handle numbers (literals)
+      | isDigit x || x == '.' =
+          if expect == ExpectLit
+            then parseNumber (x:xs) temp stack
+            else Left OperatorOrClosingParenExpected
+
+      -- Handle operators
+      | otherwise =
+          if expect == ExpectOp
+            then parseOperator x xs temp stack
+            else Left LiteralOrOpeningParenExpected
+
+    -- Parsing numbers
+    parseNumber s temp stack =
+      let (numStr, rest) = span isNumChar s
+          numMaybe = readMaybe numStr
+      in case numMaybe of
+          Just num -> tokenize (TokLit num : temp) rest ExpectOp stack
+          Nothing -> Left ParseError
+
+    -- Parsing operators
+    parseOperator c xs temp stack
+      | Just (op, prec, dir) <- lookup c opTable =
+          tokenize (TokBinOp op c prec dir : temp) xs ExpectLit stack
+      | otherwise = Left ParseError
+
+    isNumChar c = isDigit c || c == '.'
+    parseNumber s temp stack = tokenizeNumber (span isNumChar s) temp stack
+
+    tokenizeNumber (numStr, rest) temp stack
+      | Just num <- readMaybe numStr = tokenize (TokLit num : temp) rest ExpectOp stack
+      | otherwise = Left ParseError  -- Error parsing number
+
+    parseOperator c xs temp stack
+      | Just (op, prec, dir) <- lookup c opTable = tokenize (TokBinOp op c prec dir : temp) xs ExpectLit stack
+      | otherwise = Left ParseError  -- Unknown operator
+
+    isNumChar c = isDigit c || c == '.'
+
 -- Ezt akkor vedd ki a kommentblokkból, ha a 3 pontos "A parser és az algoritmus újradefiniálása" feladatot megcsináltad.
 parseAndEvalSafe ::
     (String -> ShuntingYardResult [Tok a]) ->
@@ -128,7 +237,7 @@ sySafe :: String -> ShuntingYardResult ([Double], [Tok Double])
 sySafe = parseAndEvalSafe
   (parseSafe operatorTable)
   (\ts -> shuntingYardSafe (BrckOpen : ts ++ [BrckClose]))
--}
+
 
 {-
 -- Ezt akkor vedd ki a kommentblokkból, ha az 1 pontos "Függvénytábla és a típus kiegészítése" feladatot megcsináltad.
