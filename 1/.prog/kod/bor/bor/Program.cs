ac7cde5407f05bb9f10403e699ef7a0a135599ee
János Barnabás Restye <barna0801@gmail.com>
2024-12-09 14:07:43 +0100

    prog bor + utazas

---
+++ 1/.prog/kod/bor/bor/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Data;
+
+namespace bor {
+    internal class Program {
+        public struct T {
+            public int term, ar;
+        }
+        static void Main (string[] args) {
+            // deklaralas, beolvasas
+            int i;
+            string[] temp = new string[2];
+            int n;
+            int.TryParse(Console.ReadLine(), out n);
+
+            T[] bor = new T[n + 1];
+            int minert;
+            int nagyevdb;
+            int[] nagyev = new int[n + 1];
+            int termmin;
+            int nagyevmax;
+            int kulondb;
+            int rekorddb;
+            int[] rekord = new int[n + 1];
+
+            for (i = 1; i <= n; ++i) {
+                temp = Console.ReadLine().Split(' ');
+
+                int.TryParse(temp[0], out bor[i].term);
+                int.TryParse(temp[1], out bor[i].ar);
+            }
+
+            // feladat
+            a1(out termmin, out minert, bor, i, n);
+            b1(out nagyevdb, i, n, bor, ref nagyev);
+            b2(out nagyevmax, nagyev, i, nagyevdb);
+            c1(out kulondb, i, n, bor);
+            d1(out rekorddb, i, n, ref rekord, bor);
+
+            // kiiras
+            Console.WriteLine(termmin);
+            Console.WriteLine(nagyevmax);
+            Console.WriteLine(kulondb);
+            Console.Write(rekorddb);
+            for(i = 1; i <= rekorddb; ++i) {
+                Console.Write("{0} ", rekord[i]);
+            }
+        }
+
+        static void a1 (out int termmin, out int minert, T[] bor, int i, int n) {
+            termmin = 1;
+            minert = bor[1].term;
+            for (i = 1+1; i <= n; ++i) {
+                if (minert > bor[i].term) {
+                    minert = bor[i].term;
+                    termmin = i;
+                }
+            }
+        }
+
+        static void b1 (out int nagyevdb, int i, int n, T[] bor, ref int[] nagyev) {
+            nagyevdb = 0;
+            for(i = 1; i <= n; ++i) {
+                if (bor[i].term > 1000) {
+                    ++nagyevdb;
+                    nagyev[nagyevdb] = bor[i].ar;
+                }
+            }
+        }
+
+        static void b2(out int nagyevmax, int[] nagyev, int i, int nagyevdb) {
+            nagyevmax = nagyev[1];
+            for(i = 1+1; i <= nagyevdb; ++i) {
+                if(nagyevmax < nagyev[i]) {
+                    nagyevmax = nagyev[i];
+                }
+            }
+        }
+
+        static bool voltmar(int ind, int i, T[] bor) {
+            i = 1;
+            while(i <= ind - 1 && bor[i].ar != bor[ind].ar) {
+                ++i;
+            }
+            return i > ind - 1;
+        }
+
+        static void c1 (out int kulondb, int i, int n, T[] bor) {
+            kulondb = 0;
+            for (i = 1; i <= n; ++i) {
+                if (voltmar(i, i, bor)) {
+                    ++kulondb;
+                }
+            }
+        }
+
+        static bool ujrekord(int ind, int i, T[] bor) {
+            i = 1;
+            while(i <= ind - 1 && bor[i].term < bor[ind].term) {
+                ++i;
+            }
+            return i > ind - 1;
+        }
+
+        static void d1 (out int rekorddb, int i, int n, ref int[] rekord, T[] bor) {
+            rekorddb = 0;
+            for(i = 1; i <= n; ++i) {
+                if(ujrekord(i, i, bor)) {
+                    ++rekorddb;
+                    rekord[rekorddb] = i;
+                }
+            }
+        }
+    }
+}
---
+++ 1/.prog/kod/utazas/utazas/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace utazas {
+    internal class Program {
+        public struct Utazas {
+            public int tav, ar;
+        }
+
+        static void Main (string[] args) {
+            // deklaralas
+            int i;
+            string[] temp = new string[2];
+            int n, kozelidb, tavmaxind, dragamax, hanyfele, aranydb;
+
+            int.TryParse(Console.ReadLine(), out n);
+            Utazas[] jegy = new Utazas[n + 1];
+            int[] kozeli = new int[n + 1];
+            int[] hanyszor = new int[n + 1];
+            int[] arany = new int[n + 1];
+
+            for (i = 1; i <= n; ++i) {
+                temp = Console.ReadLine().Split(' ');
+                int.TryParse(temp[0], out jegy[i].tav);
+                int.TryParse(temp[1], out jegy[i].ar);
+            }
+
+            // feladatmegoldas
+            a(out tavmaxind, i, n, jegy);
+            b1(out kozelidb, ref kozeli, i, n, jegy);
+            b2(out dragamax, kozeli, i, n);
+            c1(i, n, ref hanyszor, jegy);
+            c2(out hanyfele, i, n, hanyszor);
+            d(out aranydb, i, n, jegy, ref arany);
+
+            // kiiras
+            Console.WriteLine(tavmaxind);
+            Console.WriteLine(dragamax);
+            Console.WriteLine(hanyfele);
+            Console.Write("{0} ", aranydb);
+            if (aranydb > 0) {
+                for(i = 1; i <= aranydb; ++i) {
+                    Console.Write("{0} ", arany[i]);
+                }
+            }
+        }
+
+        static void a(out int tavmaxind, int i, int n, Utazas[] jegy) {
+            tavmaxind = 1;
+            int maxert = jegy[1].tav;
+            for (i = 1 + 1; i <= n; ++i) {
+                if (jegy[i].tav > maxert) {
+                    tavmaxind = i;
+                    maxert = jegy[i].tav;
+                }
+            }
+        }
+
+        static void b1(out int kozelidb, ref int[] kozeli, int i, int n, Utazas[] jegy) {
+            kozelidb = 0;
+            for(i = 1; i <= n; ++i) {
+                if (jegy[i].tav < 1000) {
+                    ++kozelidb;
+                    kozeli[kozelidb] = jegy[i].ar;
+                }
+            }
+        }
+
+        static void b2(out int dragamax, int[] kozeli, int i, int n) {
+            dragamax = kozeli[1];
+            for(i = 1; i <= n; ++i) {
+                if (kozeli[i] > dragamax) {
+                    dragamax = kozeli[i];
+                }
+            }
+        }
+
+        static int ismetlodik(int ind, int i, int n, Utazas[] jegy) {
+            int db = 0;
+            for(i = 1; i <= n; ++i) {
+                if (jegy[ind].ar == jegy[i].ar) {
+                    ++db;
+                }
+            }
+            return db;
+        }
+
+        static void c1(int i, int n, ref int[] hanyszor, Utazas[] jegy) {
+            for(i = 1; i <= n; ++i) {
+                hanyszor[i - 1 + 1] = ismetlodik(i, i, n, jegy);
+            }
+        }
+
+        static void c2(out int hanyfele, int i, int n, int[] hanyszor) {
+            hanyfele = 0;
+            for(i = 1; i <= n; ++i) {
+                if (hanyszor[i] == 1) {
+                    ++hanyfele;
+                }
+            }
+        }
+
+        static void d(out int aranydb, int i, int n, Utazas[] jegy, ref int[] arany) {
+            aranydb = 0;
+            for(i = 1; i <= n; ++i) {
+                if (jegy[i].ar / jegy[i].tav > 100) {
+                    ++aranydb;
+                    arany[aranydb] = i;
+                }
+            }
+        }
+    }
+}
