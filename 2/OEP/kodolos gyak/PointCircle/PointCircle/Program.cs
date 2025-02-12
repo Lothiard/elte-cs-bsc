ff3b8763ee423262434ddae285d33adfb119bb84
János Barnabás Restye <barna0801@gmail.com>
2025-02-12 22:04:40 +0100

    oep kodolos 1 & vs gitignore

---
+++ 2/OEP/kodolos gyak/PointCircle/PointCircle/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace PointCircle {
+    internal class Program {
+        static void Main(string[] args) {
+            // Thread.CurrentThread.CurrentCulture = new CultureInfo(hu-HU);
+            
+            List<Point> points = new List<Point>();
+            Circle circle;
+
+            using StreamReader sr = new("input.txt");
+
+            string[] circleParts = sr.ReadLine()!.Split(' ');
+            int circleCount = int.Parse(sr.ReadLine()!);
+            circle = new Circle(
+                new Point(
+                    double.Parse(circleParts[0]),
+                    double.Parse(circleParts[1])
+                ),
+                double.Parse(circleParts[2])
+            );
+
+            for(int i = 0; i < circleCount; ++i) {
+                string[] pointCoords = sr.ReadLine()!.Split();
+                points.Add(
+                    new Point(
+                        int.Parse(pointCoords[0]),
+                        int.Parse(pointCoords[1])
+                    )
+                );
+            }
+
+            Console.WriteLine(circle);
+            foreach (Point point in points) {
+                Console.WriteLine(point);
+            }
+
+            Console.WriteLine($"A pontot: {points[3]} tartalmazza a kör: {circle.Contains(points[3])}");
+            Console.WriteLine($"A pontot: {points[4]} tartalmazza a kör: {circle.Contains(points[4])}");
+
+            circle.Contains(points[3]);
+            circle.Contains(points[4]);
+        }
+    }
+}
