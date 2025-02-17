a3ba719a1699c8d1123310cd4262b4c9200126fc
János Barnabás Restye <barna0801@gmail.com>
2025-02-17 17:24:40 +0100

    oep kisbeadando 1 felkesz (?)

---
+++ 2/OEP/kodolos gyak/Adagolo/Adagolo/Adagolo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adagolo {
+    public class SzappanOverflow : Exception { };
+    internal class Adagolo {
+        private int _tele;
+        private int _adag;
+        private int _akt;
+
+        public Adagolo(int k, int e) {
+            _tele = k;
+            _adag = e;
+            _akt = 0;
+            if(!(_akt <= _tele)) {
+                throw new SzappanOverflow();
+            }
+        }
+
+        private int Max (int a, int b) {
+            if (a >= b) {
+                return a;
+            } else {
+                return b;
+            }
+        }
+
+        public void Nyom() {
+            _akt = Max(_akt - _adag, 0);
+        }
+
+        public void Feltolt() {
+            _akt = _tele;
+        }
+    }
+}
---
+++ 2/OEP/kodolos gyak/Adagolo/Adagolo/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Adagolo {
+    internal class Program {
+        static void Main(string[] args) {
+            
+        }
+    }
+}
