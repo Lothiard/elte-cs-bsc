10245cc09a481432d3156613502ea59ab54a5900
János Barnabás Restye <barna0801@gmail.com>
2025-03-01 08:22:07 +0100

    OEP gy3 + hazi2

---
+++ 2/OEP/beadando/Labirynth/Labirynth.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HF1 {
+    public class IllegalDimensionException : Exception { }
+    public class IllegalMoveException : Exception { }
+    public class IllegalCollect : Exception { }
+
+    public enum Content { EMPTY, WALL, GHOST, TREASURE } // allcaps?
+
+    public record struct Direction {
+        public int x, y;
+    }
+    internal class Labirynth {
+        private int _n;
+        private int _m;
+        private Content[,] _map;
+
+        public Labirynth (Content[,] map) {
+            this._n = map.GetLength(0);
+            if (!(this._n >= 0)) {
+                throw new IllegalDimensionException();
+            }
+            this._m = map.GetLength(1);
+            if (!(this._m >= 0)) {
+                throw new IllegalDimensionException();
+            }
+            this._map = map;
+        }
+
+        public Content LookAt(int x, int y, Direction dir) {
+            if (!(x + dir.x >= 1 && x + dir.x <= this._n && y + dir.y >= 1 && y + dir.y <= this._m)) {
+                throw new IllegalMoveException();   
+            }
+            return this._map[x + dir.x, y + dir.y];
+        }
+
+        public void Collect (int x, int y) {
+            if (this._map[x, y] != Content.TREASURE) {
+                throw new IllegalCollect();
+            }
+            this._map[x, y] = Content.EMPTY;
+        }
+    }
+}
