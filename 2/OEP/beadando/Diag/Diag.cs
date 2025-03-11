f3f21568177dc2a12c6b7f6b18d7ec8d446e20af
Restye János Barnabás <barna0801@gmail.com>
2025-03-11 17:28:39 +0100

    oep hazi4

---
+++ 2/OEP/beadando/Diag/Diag.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HF3 {
+    public class IllegalSizeException : Exception { }
+    public class IndexOutOfRangeException : Exception { }
+    public class IllegalIndexException : Exception { }
+
+    class Diag {
+        private double[] _x;
+        public Diag (int n) {
+            if (n < 1) { 
+                throw new IllegalSizeException();
+            }
+            _x = new double[n];
+        }
+
+        public double Get (int i, int j) {
+            if (i < 1 || i > _x.Length || j < 1 || j > _x.Length) {
+                throw new IndexOutOfRangeException();
+            }
+            if (i == j) {
+                return _x[i];
+            } else {
+                return 0.0;
+            }
+        }
+
+        public void Set (int i, int j, double e) {
+            if (i < 1 || i > _x.Length || j < 1 || j > _x.Length) {
+                throw new IndexOutOfRangeException();
+            }
+            if (i == j) {
+                this._x[i] = e;
+            } else {
+                throw new IllegalIndexException();
+            }
+        }
+
+        public static Diag Add (Diag a, Diag b) {
+            if(a._x.Length != b._x.Length) {
+                throw new IllegalSizeException();
+            }
+            Diag c = new Diag(a._x.Length);
+            for (int i = 1; i <= c._x.Length; ++i) {
+                c._x[i - 1] = a._x[i - 1] + b._x[i - 1];
+            }
+            return c;
+        }
+
+        public static Diag Multiply (Diag a, Diag b) { // a diagrammon Mul
+            if (a._x.Length != b._x.Length) {
+                throw new IllegalSizeException();
+            }
+            Diag c = new Diag(a._x.Length);
+            for (int i = 1; i <= c._x.Length; ++i) {
+                c._x[i] = a._x[i] * b._x[i];
+            }
+            return c;
+        }
+
+        public static Diag operator + (Diag a, Diag b) {
+            return Add(a, b);
+        }
+
+        public static Diag operator * (Diag a, Diag b) {
+            return Multiply(a, b);
+        }
+
+    }
+}
--- 2/OEP/kodolos gyak/Racionalis/Racionalis/Rac.cs
+++ 2/OEP/kodolos gyak/Racionalis/Racionalis/Rac.cs
@@ -10,34 +10,34 @@ namespace Rational {
         private int _n;
         private int _d;
 
-        public Rac (int i, int j) {
-            if(j == 0) {
+        public Rac (int i,int j) {
+            if (j == 0) {
                 throw new NullDivisionException();
             }
             _n = i;
             _d = j;
         }
 
-        public static Rac operator + (Rac a, Rac b) {
-            return new Rac(a._n * b._d + a._d * b._n, a._d * b._d);
+        public static Rac operator + (Rac a,Rac b) {
+            return new Rac(a._n * b._d + a._d * b._n,a._d * b._d);
         }
 
-        public static Rac operator - (Rac a, Rac b) {
-            return new Rac(a._n * b._d - a._d * b._n, a._d * b._d);
+        public static Rac operator - (Rac a,Rac b) {
+            return new Rac(a._n * b._d - a._d * b._n,a._d * b._d);
         }
 
-        public static Rac operator * (Rac a, Rac b) {
-            return new Rac(a._n * b._n, a._d * b._d);
+        public static Rac operator * (Rac a,Rac b) {
+            return new Rac(a._n * b._n,a._d * b._d);
         }
 
-        public static Rac operator / (Rac a, Rac b) {
-            if(b._n == 0) {
+        public static Rac operator / (Rac a,Rac b) {
+            if (b._n == 0) {
                 throw new NullDivisionException();
             }
-            return new Rac(a._n * b._d, a._d * b._n);
+            return new Rac(a._n * b._d,a._d * b._n);
         }
 
-        public override string ToString() {
+        public override string ToString () {
             return $"Rac:({_n},{_d})";
         }
     }
