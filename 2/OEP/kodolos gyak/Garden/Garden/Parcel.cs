0f94b5031e488e6ea0ff68b3629bf991976144a4
Restye János Barnabás <barna0801@gmail.com>
2025-04-02 21:23:07 +0200

    oep gyak VIII

---
+++ 2/OEP/kodolos gyak/Garden/Garden/Parcel.cs
@@ -0,0 +1,29 @@
+﻿namespace Garden {
+    class Parcel {
+        class ParcelIsAlreadyInUseException : Exception { }
+
+        private int _plantingTime;
+        private PlantType? _content;
+
+        public Parcel () {
+            _plantingTime = 0;
+            _content = null; // default de biztos ami biztos
+        }
+
+        public void Plant (PlantType plant, int month) {
+            if (_content != null) {
+                throw new ParcelIsAlreadyInUseException();
+            }
+            _content = plant;
+            _plantingTime = month;
+        }
+
+        public bool HasRipened (int month) {
+            return _content != null && _content is Vegetable && month - _plantingTime == _content.RipeningTime;
+        }
+
+        public void Harvest () {
+            _content = null;
+        }
+    }
+}
---
+++ 2/OEP/kodolos gyak/Garden/Garden/PlantType.cs
@@ -0,0 +1,58 @@
+﻿namespace Garden {
+    abstract public class PlantType {
+        private readonly int _ripeningTime;
+
+        public int RipeningTime => _ripeningTime;
+
+        public PlantType (int ripeningTime) {
+            _ripeningTime = ripeningTime;
+        }
+    }
+
+    // novenyfajtak
+    abstract public class Vegetable : PlantType {
+        public Vegetable (int i) : base(i) { }
+    }
+
+    abstract public class Flower : PlantType {
+        public Flower (int i) : base(i) { }
+
+    }
+
+    // novenyek
+    class Potato : Vegetable {
+        private static Potato? _instance; // null by default
+        public static Potato Instance => _instance ??= new Potato(6);
+        private Potato (int i) : base(i) { }
+    }
+
+    class Pea : Vegetable {
+        private static Pea? _instance; // null by default
+        public static Pea Instance => _instance ??= new Pea(5);
+        private Pea (int i) : base(i) { }
+    }
+
+    class Onion : Vegetable {
+        private static Onion? _instance; // null by default
+        public static Onion Instance => _instance ??= new Onion(4);
+        private Onion (int i) : base(i) { }
+    }
+
+    class Tulip : Flower {
+        private static Tulip? _instance; // null by default
+        public static Tulip Instance => _instance ??= new Tulip(3);
+        private Tulip (int i) : base(i) { }
+    }
+
+    class Carnation : Flower {
+        private static Carnation? _instance; // null by default
+        public static Carnation Instance => _instance ??= new Carnation(2);
+        private Carnation (int i) : base(i) { }
+    }
+
+    class Rose : Flower {
+        private static Rose? _instance; // null by default
+        public static Rose Instance => _instance ??= new Rose(1);
+        private Rose (int i) : base(i) { }
+    }
+}
