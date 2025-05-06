f73e0add2edc840203d1e75be1d983b844315e36
Restye János Barnabás <barna0801@gmail.com>
2025-05-06 14:52:46 +0200

    fixed little oopsie plagarism

--- 2/OEP/kisbeadando/hf10/XL.cs
+++ 2/OEP/kisbeadando/hf10/XL.cs
@@ -2,10 +2,8 @@ namespace HF10 {
     public class XL : Size {
         private static XL instance = null;
         
-        // Private constructor for singleton pattern
         private XL() { }
         
-        // XLtatic Instance method that returns the singleton instance
         public static XL Instance() {
             if (instance == null) {
                 instance = new XL();
