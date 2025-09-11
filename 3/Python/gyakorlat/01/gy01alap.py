3f60127f385dbeeed1bfd6074b9f9bb63d177562
Restye János Barnabás <barna0801@gmail.com>
2025-09-11 09:46:43 +0200

    python gy1

---
+++ 3/Python/gyakorlat/01/gy01alap.py
@@ -0,0 +1,61 @@
+# name = input("Hogy hivnak testverem ")
+# print(f"szia {name}")
+
+n = input("Enter number")
+print(n)
+print(n*3) # ha nem 101010 kell hanem 30 akkor n = int(input("Enter number"))
+print(type(n))
+print(isinstance(n, int))
+
+print(int(3.1415)) # truncates
+
+print(tuple("szia")); # lehet egyedul is a tuple
+
+data = list([1, "2025", "hello", 3.1415, 1 + 3j, [27], {1, 2, 3, "text"}, (1,), 13])
+print(type(data[0]))
+print(type(data[1]))
+print(type(data[2]))
+print(type(data[3]))
+print(type(data[4]))
+print(type(data[5]))
+print(type(data[6]))
+print(type(data[7]))
+print(type(data[8]))
+
+# alap aritmetika
+# +, -, /, %, *, //, **
+
+# osszehasonlitas
+# ==, !=, <, >, <=, >=
+
+# logika
+# or, and, not
+
+# Muveleti sorrend
+# ugyanugyaz(?)
+# TODO: utananezni precedencianak
+
+
+# adatstrukturak
+# Set
+#   add, remove, in
+
+# Dictionary
+#   dict["name"]
+#   dict.items()
+#   dict.keys()
+#   dict.values()
+
+# list
+#   slicing
+#   copying
+#   stb
+
+# string
+#   count
+#   join
+#   stb
+
+# valtozo nem kezdodhet szammal
+# csak alfanumerikus lehet, _ elfogadott
+# case sensitive
