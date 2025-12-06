f5e37ad7c37ecaae50e6112d802c2c44b6b03082
Restye János Barnabás <barna0801@gmail.com>
2025-12-06 14:34:58 +0100

    clean viewmodelbase

--- 3/Event-driven applications/beadando/3 - Avalonia UI/Tetris.Avalonia/Tetris.Avalonia/ViewModels/ViewModelBase.cs
+++ 3/Event-driven applications/beadando/3 - Avalonia UI/Tetris.Avalonia/Tetris.Avalonia/ViewModels/ViewModelBase.cs
@@ -1,34 +1,11 @@
-﻿using System;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Tetris.Avalonia.ViewModels
 {
     /// <summary>
     /// Nézetmodell ősosztály típusa.
     /// </summary>
-    public abstract class ViewModelBase : INotifyPropertyChanged
+    public class ViewModelBase : ObservableObject
     {
-        /// <summary>
-        /// Nézetmodell ősosztály példányosítása.
-        /// </summary>
-        protected ViewModelBase() { }
-
-        /// <summary>
-        /// Tulajdonság változásának eseménye.
-        /// </summary>
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        /// <summary>
-        /// Tulajdonság változása ellenőrzéssel.
-        /// </summary>
-        /// <param name="propertyName">Tulajdonság neve.</param>
-        protected virtual void OnPropertyChanged([CallerMemberName] String? propertyName = null)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
     }
 }
