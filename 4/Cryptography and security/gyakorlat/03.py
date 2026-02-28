ac23f266a65de14c4d4a2b2c12812c4f702b58e5
Restye János Barnabás <barna0801@gmail.com>
2026-02-28 09:13:59 +0100

    cryptography gy03 kommentekkel

--- 4/Cryptography and security/gyakorlat/03.py
+++ 4/Cryptography and security/gyakorlat/03.py
@@ -1,31 +1,23 @@
 from string import ascii_lowercase, ascii_uppercase
 import random
-import re
-from math import gcd, comb
-from collections import Counter
-
-# SHIFT CIPHER (ELTOLÁSOS TITKOSÍTÁS)
 
 
+# ELTOLÁSOS TITKOSÍTÁS
 def shift_cipher_gen() -> int:
-    return random.randint(1, 26)
+    return random.randint(0, 25)
 
 
 def shift_cipher_enc(key: int, pt: str) -> str:
     ct = ""
-
     for c in pt:
         ct += ascii_uppercase[(ascii_lowercase.find(c) + key) % 26]
-
     return ct
 
 
 def shift_cipher_dec(key: int, ct: str) -> str:
     pt = ""
-
     for c in ct:
         pt += ascii_lowercase[(ascii_uppercase.find(c) - key) % 26]
-
     return pt
 
 
@@ -35,8 +27,6 @@ print(shift_cipher_enc(13, shift_cipher_enc(13, "teszt").lower()).lower() == "te
 
 
 # HELYETTESÍTÉSES TITKOSÍTÁS
-
-
 def subst_gen() -> dict[str, str]:
     perm = random.sample(ascii_lowercase, k=26)
     return {k: v for k, v in zip(ascii_lowercase, perm)}
@@ -56,7 +46,6 @@ def subst_dec(key: dict[str, str], ct: str) -> str:
 
 k = subst_gen()
 print(subst_dec(k, subst_enc(k, "teszt")) == "teszt")
-
 
 # forrás: http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
 freq = {
@@ -100,7 +89,7 @@ subst_ct = (
 )
 
 
-# VIGENERE-FÉLE TITKOSÍTÁS
+# VIGENERE-FÉLE TCHRITKOSÍTÁS
 def repeat_to_length(s: str, length: int) -> str:
     return (s * length)[:length]
 
@@ -120,14 +109,6 @@ def vigenere_enc(key: str, pt: str) -> str:
         ]
     return ct
 
-    # key = repeat_to_length(key, len(pt))
-    # ct = []
-    # for p, k in zip(pt, key):
-    #     offset = ord("A") if p.isupper() else ord("a")
-    #     c = chr((ord(p) - offset + ord(k.lower()) - ord("a")) % 26 + offset)
-    #     ct.append(c)
-    # return "".join(ct)
-
 
 def vigenere_dec(key: str, ct: str) -> str:
     pt = ""
@@ -136,14 +117,6 @@ def vigenere_dec(key: str, ct: str) -> str:
             (ascii_uppercase.find(c) - ascii_lowercase.find(key[i])) % 26
         ]
     return pt
-
-    # key = repeat_to_length(key, len(ct))
-    # pt = []
-    # for c, k in zip(ct, key):
-    #     offset = ord("A") if c.isupper() else ord("a")
-    #     p = chr((ord(c) - offset - (ord(k.lower()) - ord("a"))) % 26 + offset)
-    #     pt.append(p)
-    # return "".join(pt)
 
 
 pt = "hellotesztszoveghello"
@@ -162,7 +135,11 @@ vigenere_ct = (
     "WQAIIWXNRMGWOIIFKEE"
 )
 
+
 kasiski = []
+import re
+from math import gcd
+
 matches = re.finditer("CHR", vigenere_ct, re.MULTILINE)
 for match in matches:
     print(match.start())
@@ -170,17 +147,44 @@ for match in matches:
 print(gcd(*kasiski))
 
 
+from math import comb
+from collections import Counter
+
+
+# Index of coincidence: megadja annak a valószínűségét, hogy az `s` sztring-ben két
+# véletlenszerűen választott karakter megegyezik.
+# Minden i = 0,1,...,25-re legyen `fi` az ábécé i-edik karakterének előfordulása az `s` sztringben
+# Minden i = 0,1,...,25-re pontosan comb(fi, 2)=fi*(fi-1)/2 féleképpen tudjuk kiválasztani az i-edik
+# karaktert az ábécéből az `s` sztringben.
+# És pontosan comb(n, 2)=n*(n-1)/2 féleképpen tudunk két tetszőleges karaktert kiválasztani az `s` sztringben
+# Annak a valószínűsége, hogy két megegyező karaktert választunk `s`-ben, az egyes karakterek kiválasztási
+# valószínűségének összege, osztva az összes lehetséges esettel:
 def index_of_coincidence(s: str) -> float:
     return sum(comb(fi, 2) for fi in Counter(s).values()) / comb(len(s), 2)
 
 
 print(
     index_of_coincidence(
-        "thisisaverylongtextbecausehopefullyitwillshowwhatiwantittoshow"
+        "thisisaverylongtextbecausehopefullyitwillshowwhatiwantoshowandstatisticsisfunbycryptographyismorefun"
     )
 )
 print(index_of_coincidence("".join(random.choices(ascii_lowercase, k=1000))))
+# Értelmes angol szövegnek az index_of_coincidence értéke 0.068 körül van
+# Véletlen angol karaktersorozatnak az index_of_coincidence értéke 0.038 körül van
 
+k = 5
+c_k = [vigenere_ct[s::k] for s in range(k)]
+print(c_k)
+
+# Legyen k=1. Keressük meg a kulcs hosszát:
+# - a titkos szöveget felosztjuk k részre, minden rész minden k-adik karaktert tartalmaz:
+#   c_1 = c_1 c_{1+k} c_{1+2k} ...
+#   c_2 = c_2 c_{2+k} c_{2+2k} ...
+#   ...
+#   c_k = c_k c_{2k} c_{3k} ...
+# - minden résznek kiszámoljuk az index_of_coincidence értékét
+# - ha minden rész index_of_coincidence értéke ~0.068, akkor k megadja a kulcs hosszát
+# - ha nem minden rész index_of_coincidence értéke ~0.068, akkor köveljük k értékét egyel és kezdjük elölről
 k = 1
 while True:
     c_k = [index_of_coincidence(vigenere_ct[s::k]) for s in range(k)]
@@ -188,9 +192,15 @@ while True:
         break
     k += 1
 
-print("A kulcs hossza valoszinuleg", k)
+print("A kulcs hossza valószínűleg", k)
 
-
+# Ha tudjuk a kulcs hosszát (k), akkor minden m karakterre a kulcsban végezzük el a következőt:
+# - osszuk fel a ciphertext-et: c_m c_{m+k} c_{m+2k}... karaktereket tartalmazza
+# - toljunk el a részsztringet 0 <= g <= 25-tel, és definiáljuk M_g = sum_{i=0}^{25} p_i f_{i+g} / n értéket,
+#   ahol p_i az ábécé i-edik karakterének előfordulásának valószínűsége, n a részsztring hossza
+#   és f_{i+g} az ábécé i-edik karakterének g-vel eltolt előfodulása a részsztringben
+#   (pl: i = 1 és g = 2: f_1 = B és f_{1+2} = f_3 = D előfordulását nézzük a részsztring-ben B helyett)
+# - állítás: ha M_g ~ 0.068, akkor az ábécé g-edik karaktere lesz a helyes karakter ebben az iterációban
 key = ""
 for m in range(k):
     c_m = vigenere_ct[m::k]
@@ -204,4 +214,5 @@ for m in range(k):
         if 0.055 <= idc <= 0.075 and max_idc[1] <= idc:
             max_idc = (ascii_lowercase[g], idc)
     key += max_idc[0]
-print("Potencialis kulcs: ", key)
+print("Potenciális kulcs: ", key)
+print(vigenere_dec(repeat_to_length(key, len(vigenere_ct)), vigenere_ct))
