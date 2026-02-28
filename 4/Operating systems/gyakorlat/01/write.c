e0625fd0c32356c4d245d170b8b83971a0e706a7
Restye János Barnabás <barna0801@gmail.com>
2026-02-28 09:49:38 +0100

    OS gy01 format

--- 4/Operating systems/gyakorlat/01/write.c
+++ 4/Operating systems/gyakorlat/01/write.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-//call it with some parameters from the command line 
+// call it with some parameters from the command line
 
-int main(int argc,char ** argv) 
-//char** means an array of character arrays = array of strings
+int main(int argc, char** argv)
+// char** means an array of character arrays = array of strings
 {
- int i;
+    int i;
 
- for (i=0;i<atoi(argv[2]);i++){
-
-  printf("%s\n",argv[1]);
- }
- return 0;
+    for (i = 0; i < atoi(argv[2]); i++) { printf("%s\n", argv[1]); }
+    return 0;
 }
